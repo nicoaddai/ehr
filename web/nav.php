2e4596d187044f13427b00face3e4624b59f1766
KevanGP <kevgp0@gmail.com>
2023-08-16 13:16:44 -0500

    Adding some basic web controls, we may use something different.

---
+++ web/nav.php
@@ -0,0 +1,10 @@
+<html>
+<h2>EHR navigation</h2>
+<ul>
+<li><a href="index.php">Home</a></li>
+<li><a href="add.php">Add record</a></li>
+<li><a href="list.php">Record List</a></li>
+<li><a href="https://github.com/kevangp/ehr" target="_blank">GitHub (External)</a></li>
+</ul>
+<hr>
+</html>
